d3a0aded4c5b6f76b93d4ec927cfe933f71d035b
li <li@163.com>
2022-04-18 23:34:46 +0800

    电影购票系统登陆及商家下架、修改商品

--- src/main/java/employee/MovieBuy/MovieSystem.java
+++ src/main/java/employee/MovieBuy/MovieSystem.java
@@ -3,6 +3,7 @@ package main.java.employee.MovieBuy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -100,6 +101,7 @@ public class MovieSystem {
                     break;
                 default:
                     System.out.println("请选择正确的操作！！");
+                    LOGGER.trace("未登录，选择操作错误！");
                     break;
             }
         }
@@ -118,16 +120,20 @@ public class MovieSystem {
                     loginUser = user;
                     // 判断是商家登陆还是用户登陆
                     if (user instanceof Customer){
+                        LOGGER.trace("用户：" + loginUser.getLoginName() + "登陆成功！");
                         showCustomerMain();
                     }else {
+                        LOGGER.trace("商家：" + loginUser.getLoginName() + "登陆成功！");
                         showBusinessMain();
                     }
                     return;
                 }else {
                     System.out.println("密码错误，请重新输入！");
+                    LOGGER.trace("密码输入错误！");
                 }
             }else {
                 System.out.println("登录名输入有误，请重新输入！");
+                LOGGER.trace("登录名输入错误！");
             }
         }
 
@@ -154,22 +160,31 @@ public class MovieSystem {
                         addMovies();
                     } catch (Exception e) {
                         e.printStackTrace();
-                        LOGGER.error("添加电影失败" + e);
+                        LOGGER.error(loginUser.getLoginName() + "添加电影失败" + e);
                     }
                     break;
                 case "3":
+                    deleteMovies();
                     break;
                 case "4":
+                    try {
+                        upDateMovies();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        LOGGER.trace(loginUser.getLoginName()+ "修改电影失败：" + e);
+                    }
                     break;
                 case "5":
+                    LOGGER.trace("商家：" + loginUser.getLoginName() + "退出登陆");
                     return;
                 default:
                     System.out.println("请选择正确的操作！");
-
+                    LOGGER.trace(loginUser.getLoginName() + "商家选择操作错误！");
             }
         }
 
     }
+
     // 普通用户界面
     private static void showCustomerMain() {
         while (true) {
@@ -192,9 +207,11 @@ public class MovieSystem {
                 case "4":
                     break;
                 case "5":
+                    LOGGER.trace("用户：" + loginUser.getLoginName() + "退出登陆");
                     return;
                 default:
                     System.out.println("请选择正确的操作！");
+                    LOGGER.trace(loginUser.getLoginName() + "用户选择操作错误！");
 
             }
         }
@@ -202,15 +219,17 @@ public class MovieSystem {
     }
 
     public static User getUserByLoginName(String loginName){
+        LOGGER.trace("开始登陆");
         // 判断用户集合中是否存在输入的用户名
         for (User user : ALL_USERS) {
             if (loginName.equals(user.getLoginName())){
                 return user;
             }
         }
+        LOGGER.trace("用户不存在");
         return null;
     }
-    // 查询所有本商家的电影
+    // 商家查询所有本商家的电影
     private static void selectMovies() {
         System.out.println("电影名称    主演    评分    时长    价格    余票    放映时间    ");
        // 先获取所有的key
@@ -221,12 +240,12 @@ public class MovieSystem {
                 for (Movie ls : ALL_MOVIES.get(bus)) {
                     System.out.println(ls);
                 }
-            }else {
-                if (loginUser.getLoginName().equals("shangjia2")&&loginUser.getLoginName().equals(bus.getLoginName())){
+                LOGGER.trace(loginUser.getLoginName() + "查询电影成功");
+            }else if(loginUser.getLoginName().equals("shangjia2")&&loginUser.getLoginName().equals(bus.getLoginName())){
                     for (Movie ls : ALL_MOVIES.get(bus)) {
                         System.out.println(ls);
                     }
-                }
+                LOGGER.trace(loginUser.getLoginName() + "查询电影成功");
             }
         }
     }
@@ -264,12 +283,10 @@ public class MovieSystem {
         //将添加好的电影对象根据商家登陆名放入指定的list
         if (loginUser.getLoginName().equals("shangjia1")){
             movies1.add(mo);
-            System.out.println("商家1的电影：" + movies1);
-            System.out.println("商家2的电影：" + movies2);
+            LOGGER.trace(loginUser.getLoginName() + "开始添加电影：" + movies1);
         }else {
             movies2.add(mo);
-            System.out.println("商家1的电影：" + movies1);
-            System.out.println("商家2的电影：" + movies2);
+            LOGGER.trace(loginUser.getLoginName() + "开始添加电影：" + movies2);
         }
         // 将这个list 添加到电影map中指定的上架value下
         // 先获取所有的key
@@ -278,13 +295,154 @@ public class MovieSystem {
         for (Business bus : userKeys) {
             if (loginUser.getLoginName().equals("shangjia1")&&loginUser.getLoginName().equals(bus.getLoginName())){
                 ALL_MOVIES.put(bus,movies1);
-            }else {
+                LOGGER.trace(loginUser.getLoginName() + "成功添加电影：" + movies1);
+            }else if(loginUser.getLoginName().equals("shangjia2")&&loginUser.getLoginName().equals(bus.getLoginName())){
                 ALL_MOVIES.put(bus,movies2);
+                LOGGER.trace(loginUser.getLoginName() + "成功添加电影：" + movies2);
             }
         }
         System.out.println(ALL_MOVIES);
-
-
+    }
+    // 商家下架电影
+    private static void deleteMovies() {
+        while (true) {
+            System.out.println("请输入需要下架的电影名称");
+            String inputMoviesName = ALL_sca.nextLine();
+            LOGGER.info("开始下架电影:" + inputMoviesName);
+            //定义set集合存放 All_movies 的所有key
+            Set<Business> userKey = ALL_MOVIES.keySet();
+            // 遍历userKey 找到与登陆用户相同的key
+            to:for (Business business : userKey) {
+                if (loginUser.getLoginName().equals(business.getLoginName())){
+                    //拿到当前登陆商家的所有电影的list
+                    for (Movie movie : ALL_MOVIES.get(business)) {
+                        if (inputMoviesName.equals(movie.getMovieNmae())){
+                            ALL_MOVIES.get(business).remove(movie);
+                            LOGGER.trace(loginUser.getLoginName() + "下架电影成功:" + inputMoviesName);
+                            return;
+                        }else {
+                            System.out.println("请输入正确的电影名称！");
+                            LOGGER.trace(loginUser.getLoginName() + "下架电影失败,没有该电影:" + inputMoviesName);
+                            break to;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    //商家修改电影
+    private static void upDateMovies() throws Exception {
+        while (true){
+            System.out.println("请输入需要修改的电影名称");
+            String inputMoviesName = ALL_sca.nextLine();
+            //合并两个movies集合
+            Collection<Movie> colMovies1 = new ArrayList<>(movies1);
+            Collection<Movie> colMovies2 = new ArrayList<>(movies2);
+            colMovies1.addAll(colMovies2);
+            int NO = 0;
+            for (Movie movie : colMovies1) {
+                if (!inputMoviesName.equals(movie.getMovieNmae())){
+                    NO++;
+                }
+            }
+            if (NO == colMovies1.size()){
+                System.out.println("请输入正确的电影名称！");
+                LOGGER.trace(loginUser.getLoginName()+ "修改电影失败，没有该电影1：" + inputMoviesName);
+                continue;
+            }
+            LOGGER.info(loginUser.getLoginName() + "开始修改电影:" + inputMoviesName);
+            //定义set集合存放 All_movies 的所有key
+            Set<Business> userKey = ALL_MOVIES.keySet();
+            // 遍历userKey 找到与登陆用户相同的key
+            for (Business business : userKey) {
+                if (loginUser.getLoginName().equals(business.getLoginName())){
+                    //定义计数器，用于记录循环次数
+                    int upMovieForMovies = 0;
+                    //拿到当前登陆商家的所有电影的list
+                    for (Movie movie : ALL_MOVIES.get(business)) {
+                        if (inputMoviesName.equals(movie.getMovieNmae())){
+                            System.out.println("请输入新电影名称：");
+                            String movieName = ALL_sca.nextLine();
+                            System.out.println("请输入新电影主演：");
+                            String movieLead = ALL_sca.nextLine();
+                            System.out.println("请输入新电影评分：");
+                            String movieScore = ALL_sca.nextLine();
+                            System.out.println("请输入新电影时长：");
+                            String movieLongTime = ALL_sca.nextLine();
+                            System.out.println("请输入新电影价格：");
+                            String moviePrice = ALL_sca.nextLine();
+                            System.out.println("请输入新电影余票：");
+                            String movieNumber = ALL_sca.nextLine();
+                            System.out.println("请输入新电影放映时间：");
+                            String movieStartTime = ALL_sca.nextLine();
+                            //将添加好的电影对象根据商家登陆名放入指定的list
+                            if (loginUser.getLoginName().equals("shangjia1")){
+//                                movies1.add(mo);
+                                for (Movie movie1 : movies1) {
+                                    if (inputMoviesName.equals(movie1.getMovieNmae())){
+                                        movie1.setMovieNmae(movieName);
+                                        movie1.setLead(movieLead);
+                                        //将String类型的数字转换为Double
+                                        Double dScore = new Double(movieScore);
+                                        movie1.setScore(dScore);
+                                        Double dLongTime = new Double(movieLongTime);
+                                        movie1.setLongTime(dLongTime);
+                                        Double dPrice = new Double(moviePrice);
+                                        movie1.setPrice(dPrice);
+                                        Integer iNumber = new Integer(movieNumber);
+                                        movie1.setNumber(iNumber);
+                                        //将字符串日期转换为时间格式
+                                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                                        movie1.setStartTime(sdf.parse(movieStartTime));
+                                    }
+                                }
+                            }else {
+                                for (Movie movie1 : movies2) {
+                                    if (inputMoviesName.equals(movie1.getMovieNmae())){
+                                        movie1.setMovieNmae(movieName);
+                                        movie1.setLead(movieLead);
+                                        //将String类型的数字转换为Double
+                                        Double dScore = new Double(movieScore);
+                                        movie1.setScore(dScore);
+                                        Double dLongTime = new Double(movieLongTime);
+                                        movie1.setLongTime(dLongTime);
+                                        Double dPrice = new Double(moviePrice);
+                                        movie1.setPrice(dPrice);
+                                        Integer iNumber = new Integer(movieNumber);
+                                        movie1.setNumber(iNumber);
+                                        //将字符串日期转换为时间格式
+                                        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                                        movie1.setStartTime(sdf.parse(movieStartTime));
+                                    }
+                                }
+                            }
+                            // 将这个list 添加到电影map中指定的上架value下
+                            // 先获取所有的key
+                            Set<Business> userKeys = ALL_MOVIES.keySet();
+                            //遍历map集合找到该商家的所有电影
+                            for (Business bus : userKeys) {
+                                if (loginUser.getLoginName().equals("shangjia1")&&loginUser.getLoginName().equals(bus.getLoginName())){
+                                    ALL_MOVIES.put(bus,movies1);
+                                    LOGGER.trace(loginUser.getLoginName() + "修改电影成功：" + movies1);
+                                }else if(loginUser.getLoginName().equals("shangjia2")&&loginUser.getLoginName().equals(bus.getLoginName())){
+                                    ALL_MOVIES.put(bus,movies2);
+                                    LOGGER.trace(loginUser.getLoginName() + "修改电影成功：" + movies2);
+                                }
+                            }
+                            System.out.println(ALL_MOVIES);
+                            return;
+                        }else {
+                            upMovieForMovies++;
+                        }
+                    }
+                    if (upMovieForMovies == ALL_MOVIES.get(business).size()){
+                        System.out.println("请输入正确的电影名称！");
+                        LOGGER.trace(loginUser.getLoginName() + "修改电影失败,没有该电影:" + inputMoviesName);
+                        break;
+                    }
+                }
+            }
+        }
     }
 
 }
